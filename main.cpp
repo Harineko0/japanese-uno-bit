88a12d086c3ec5c8b2bf39c3700016f9fe969e0f
Harineko0 <harineko0927@gmail.com>
2022-01-26 01:56:51 +0900

    Complete game

--- main.cpp
+++ main.cpp
@@ -2,16 +2,15 @@
 #include <bitset>
 #include <bit>
 #include <cstdint>
-#include <limits>
 
 using namespace std;
 
 const int MAX_PLAYER = 2;
 
-int trials;
-int win[MAX_PLAYER];
+__int64 trials = 0;
+__int64 win[MAX_PLAYER] = {0, 0};
 
-typedef int IntCard;
+typedef __int64 IntCard;
 
 void print_bit(int num) {
     cout << bitset<52>(num) << endl;
@@ -21,7 +20,7 @@ void print(string str) {
     cout << str << endl;
 }
 
-int MSB64bit(unsigned __int64 v) {
+int bsr(unsigned __int64 v) {
     if (v == 0) -1;
     v |= (v >> 1);
     v |= (v >> 2);
@@ -32,6 +31,17 @@ int MSB64bit(unsigned __int64 v) {
     return __popcount(v) - 1;
 }
 
+int bsf(unsigned __int64 v) {
+    if (v == 0) return -1;
+    v |= (v << 1);
+    v |= (v << 2);
+    v |= (v << 4);
+    v |= (v << 8);
+    v |= (v << 16);
+    v |= (v << 32);
+    return 64 - __popcount(v);
+}
+
 int next_order(int order) {
     order++;
     if (order >= MAX_PLAYER) {
@@ -40,51 +50,80 @@ int next_order(int order) {
     return order;
 }
 
-void next_game(int order, IntCard table, IntCard players[]) {
+void next_game(int order, IntCard table, IntCard player0, IntCard player1) {
     // check is end game
-    int playerWhoHasCard = 0;
-    for (int i = 0; i < MAX_PLAYER; i++) {
-        IntCard player = players[i];
-        if (__popcount(player) > 0) {
-            playerWhoHasCard++;
+//    int playerWhoHasCard = 0;
+//    for (int i = 0; i < MAX_PLAYER; i++) {
+//        IntCard player = players[i];
+//        if (__popcount(player) > 0) {
+//            playerWhoHasCard++;
+//        }
+//    }
+    bool firstHasCard = __popcount(player0) == 0;
+    if (firstHasCard || __popcount(player1) == 0) {
+        trials++;
+        if (firstHasCard){
+            win[0]++;
+        } else {
+            win[1]++;
         }
-    }
-    if (playerWhoHasCard < 2) {
-        print("end");
+//        print("end game");
         return;
     }
 
     // check can pass
     // プレイヤーが2人だから今出す順番の人が出せなければ(最上位ビットがテーブル以下ならば)パス
-    IntCard p = players[order];
-    bool canPass = __popcount(table) != 0 and MSB64bit(p) >> 2 <= MSB64bit(table) >> 2;
-    if (canPass) {
-        next_game(next_order(order), 0, players);
+    IntCard player;
+    if (order == 1) {
+        player = player1;
+    } else {
+        player = player0;
     }
 
-    IntCard c = players[order];
+    IntCard tableCard = bsr(table) >> 2;
+    bool canPass = __popcount(table) != 0 and bsr(player) >> 2 <= tableCard;
+
+    if (canPass) {
+//        print("pass");
+        next_game(next_order(order), 0, player0, player1);
+    }
+
+    IntCard c = player;
 
     while(c)
     {
-        IntCard ic = c & -c;
-//        print_bit(ic);
+        IntCard card = bsf(c);
+//        print("--------------------------------------------------");
+//        print(to_string(order));
+//        print_bit( order == 0 ? player : player0);
+//        print_bit( order == 1 ? player : player1);
+//        print_bit(card);
+//        print(to_string((card >> 2)) + ", " + to_string(tableCard));
+
+        if (tableCard == -1 or card >> 2 > tableCard) {
+//            print(">>>>>>>>>>>>>");
+//            print_bit( order == 0 ? player & ~(1 << card) : player0);
+//            print_bit( order == 1 ? player & ~(1 << card) : player1);
+//            print_bit(table | (1 << card));
+//            print("--------------------------------------------------");
+            next_game(next_order(order), table | (1 << card),
+                      order == 0 ? player & ~(1 << card) : player0
+                      , order == 1 ? player & ~(1 << card) : player1);
+        }
 
         c &= c - 1;
     }
 }
 
 int main() {
-    cout << "Hello, World!" << endl;
-
     int order = 0;
     IntCard table = 0;
-    IntCard players[MAX_PLAYER];
-    players[0] = 1ULL << 0 | 1ULL << 4 | 1ULL << 8;
-    players[1] = 1ULL << 0 | 1ULL << 4 | 1ULL << 8;
+    IntCard player1 = 1ULL << 0 | 1ULL << 4 | 1ULL << 8 | 1ULL << 12 | 1ULL << 16 | 1ULL << 20 | 1ULL << 24 | 1ULL << 28 | 1ULL << 32;
+    IntCard player2 = 1ULL << 0 | 1ULL << 4 | 1ULL << 8 | 1ULL << 12 | 1ULL << 16 | 1ULL << 20 | 1ULL << 24 | 1ULL << 28 | 1ULL << 32;
 
-    print_bit(players[0]);
+    next_game(order, table, player1, player2);
 
-    next_game(order, table, players);
+    print(to_string(trials) + ", " + to_string(win[0]));
 
     return 0;
 }
